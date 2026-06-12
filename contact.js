7e3fd382933a7e920e5706edb69b8cc3646b9b40
nckvmk <nicklgkl@outlook.com>
2026-06-12 17:19:13 +0300

    Added and completed contact form layout in contact.html

---
+++ contact.js
@@ -0,0 +1,7 @@
+
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('set', 'developer_id.dYzg1YT', true);
+gtag('config', 'G-SEKJ4E9T4H');
