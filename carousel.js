fc5391e4934df4dbde03cbd4ad0fbc0e4e35cdfc
nckvmk <nicklgkl@outlook.com>
2026-06-13 12:35:30 +0300

    Added video playback to carousel

--- carousel.js
+++ carousel.js
@@ -1,5 +1,7 @@
+//When recording a click from the user within the area of the carousel, the target image is opened in a new tab.
+
 $(document).ready(function() {
-    $(".carousel-item").on("click", function() {
-        window.open($(this), "_blank");
+    $(".carousel-item img").on("click", function() {
+        window.open($(this).attr("src"), "_blank");
     });
 });
