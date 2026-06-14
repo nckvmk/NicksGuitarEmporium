211f74faefb05157c93e968dc837ed20169c6513
nckvmk <nicklgkl@outlook.com>
2026-06-14 18:05:48 +0300

    More work on the tasks layout

--- tasks.js
+++ tasks.js
@@ -1,7 +1,9 @@
 /* This script is responsible for the dynamic data manipulation of the tasks table */
 
 $(document).ready(function() {
+    //Table row delete functionality
     $(".btnDelete").on('click', function() {
         $(this).closest('tr').remove();
     });
-})
+
+});
