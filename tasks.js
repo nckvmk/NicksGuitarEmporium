99a3b69c0ba4710f06f7f4266a17aa79693de71e
nckvmk <nicklgkl@outlook.com>
2026-06-18 19:53:46 +0300

    MErge reattempt

--- tasks.js
+++
@@ -1,9 +0,0 @@
-/* This script is responsible for the dynamic data manipulation of the tasks table */
-
-$(document).ready(function() {
-    //Table row delete functionality
-    $(".btnDelete").on('click', function() {
-        $(this).closest('tr').remove();
-    });
-
-});
